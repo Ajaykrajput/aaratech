8db3c0f30a776479c677ec21cd16ad6d9e98c0bf
Ajay <ajayrajput7rcr@gmail.com>
2025-02-08 15:20:52 +0530

    fixed issus

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -21,35 +21,25 @@ const EventCard = () => {
     };
   }, []);
 
-
-  echo "# aaratech" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Ajaykrajput/aaratech.git
-git push -u origin main
-
   return (
     <>
+      {/* Main Event Card */}
       <div className="fixed bottom-48 right-0 bg-gradient-to-tl from-[#a5ecd8] to-orange-400 p-4 rounded-lg shadow-lg max-w-[180px] cursor-pointer w-full z-20">
         <h3 className="text-sm font-semibold text-white">Latest Event</h3>
         <div className="flex justify-between w-full py-1">
-          <h3 className="text-sm font-semibold text-white">
-            {" "}
-            नवीनतम कार्यक्रम
-          </h3>
+          <h3 className="text-sm font-semibold text-white">नवीनतम कार्यक्रम</h3>
 
           <ChevronDown
-            className="w-4 h-4 ml-auto fill-black cursor-pointer"
+            className="w-4 h-4 ml-auto text-black cursor-pointer"
             onClick={() => setOpenEvent((prev) => !prev)}
           />
         </div>
 
+        {/* Event Details Card */}
         {openEvent && (
           <div
             ref={eventDetailsRef}
-            className="fixed bottom-64 right-0 rounded-xl max-w-2xl w-full z-50 shadow-[0_0_15px_5px_rgba(255,165,0,0.9)]"
+            className="fixed bottom-[250px] right-2 rounded-xl max-w-2xl w-full z-50 shadow-[0_0_15px_5px_rgba(255,165,0,0.9)]"
           >
             <table className="min-w-full text-sm rounded-l-md text-gray-800 text-center bg-orange-300 p-4">
               <thead className="font-semibold text-base">
@@ -69,9 +59,7 @@ git push -u origin main
                   </td>
                   <td className="px-2 py-2">गाजियाबाद</td>
                 </tr>
-              </tbody>
-              <tbody>
-                <tr className="hover:bg-white hover:z-50 hover:shadow-amber-50 shadow-2xll">
+                <tr className="hover:bg-white hover:z-50 hover:shadow-amber-50 shadow-2xl">
                   <td className="px-2 py-2">2023-07-03</td>
                   <td className="px-2 py-2">2023-07-03</td>
                   <td className="px-2 py-2">
@@ -80,8 +68,6 @@ git push -u origin main
                   </td>
                   <td className="px-2 py-2">लखनऊ</td>
                 </tr>
-              </tbody>
-              <tbody>
                 <tr className="hover:bg-white hover:z-50 hover:shadow-amber-50 shadow-2xl">
                   <td className="px-2 py-2">2023-07-15</td>
                   <td className="px-2 py-2">2023-07-15</td>
@@ -94,6 +80,7 @@ git push -u origin main
         )}
       </div>
 
+      {/* WhatsApp Icon */}
       <div className="fixed bottom-12 cursor-pointer max-w-[180px] right-0 w-full z-20">
         <img
           className="w-20 h-20 ml-auto right-5 mx-5"
